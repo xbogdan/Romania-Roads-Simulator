f8f2caa06511fc5ce1b4de455615d4af382344a9
Bodan Boamfa <bogdan.boamfa@gmail.com>
2014-05-11 03:08:13 +0300

    Add ability to move the car on different lane using arrow keys

--- js/main.js
+++ js/main.js
@@ -2,11 +2,22 @@ window.onload = function() {
     window.gameWindow = document.getElementById("game-window");
     window.gameWindow.style.backgroundPosition = '0px 0px';
     window.car = document.getElementById("car");
+    window.moveCarStep = 243;
+
     var travelSpeed = 1;
     var travelInterval = 10;
 
     travel(travelSpeed, travelInterval);
-
+    
+    document.onkeydown = function (e) {
+        e = e || window.event;
+        if (e.keyCode == 37) {
+            moveCar(1);
+        }
+        if (e.keyCode == 39) {
+            moveCar(-1);
+        }
+    };
 }
 
 function travel(travelSpeed, interval) {
@@ -23,6 +34,7 @@ function travel(travelSpeed, interval) {
 function moveCar(lane) {
     var carStyle = getComputedStyle(window.car, null)
     var lastRight = parseInt(carStyle.getPropertyValue('right'), 10);
-    var newRight = lastRight + lane * 243;
-    window.car.style.right = newRight + 'px';
+    var newRight = lastRight + lane * moveCarStep;
+    if ((newRight-77) % moveCarStep == 0 && (newRight-77) / moveCarStep <= 3 && (newRight-77) / moveCarStep >= 0)
+        window.car.style.right = newRight + 'px';
 }
