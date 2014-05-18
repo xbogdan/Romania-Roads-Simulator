63e1f4d14d9ae60fd376db9371749d0855e95268
Bodan Boamfa <bogdan.boamfa@gmail.com>
2014-05-18 18:01:52 +0300

    Created preload function; game is now adding random obstacles.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,30 @@ var travelInterval = 10;
 var leftKey = false;
 var rightKey = false;
 var carLastPos = 0;
-var carMoveStep = 3;
+var carMoveStep = 7;
 var carMoveAcc = 0;
+var images = ['road-template.png', 'car1.png', 'begger1.png', 'drunk1.png', 'drunk2.png', 'drunk3.png', 'hole1.png'];
+var obstacleImg = ['begger1.png', 'drunk1.png', 'drunk2.png', 'drunk3.png', 'hole1.png'];
+
+
 window.onload = function() {
     window.gameWindow = document.getElementById("game-window");
     window.gameWindow.style.backgroundPosition = '0px 0px';
     window.car = document.getElementById("car");
+
+    // set car position
     carLastPos = parseInt(css(window.car, 'left'), 10);
-    
-    travel(travelSpeed, travelInterval);    
-    
-    setInterval(updateKeys, 1);
+
+    // preload images
+    preload(images);
+
+    // start background movement
+    bgMovement(travelSpeed, travelInterval);    
+
+    // start updating keys
+    setInterval(updateKeys, 10);
+
+    addRandomObstacles();
 }
 
 
@@ -44,31 +57,17 @@ window.onkeyup = function (e) {
 
 
 /*
-*
+* Makes background move
 */
-function travel(travelSpeed, interval) {
+function bgMovement(travelSpeed, interval) {
     setTimeout(function() {
-        travel(travelSpeed, interval);
+        bgMovement(travelSpeed, interval);
     }, interval);
-    var gameWindowPos = window.gameWindow.style.backgroundPosition.split(' ');
+    var gameWindowPos = css(window.gameWindow, 'background-position').split(' ');
     var lastPosX = parseInt(gameWindowPos[0], 10);
     var lastPosY = parseInt(gameWindowPos[1], 10);
     var newPos = lastPosY + travelSpeed;
     window.gameWindow.style.backgroundPosition = lastPosX + 'px ' + newPos + 'px';
-}
-
-
-/*
-*
-*/
-function moveElement(element, travelSpeed, interval) {
-    setTimeout(function() {
-        moveElement(element, travelSpeed, interval);
-    }, interval);
-    var lastPosY = parseInt(element.style.bottom, 10);
-    var newPosY = lastPosY - travelSpeed;
-    element.style.bottom = newPosY + 'px';
-    return element;
 }
 
 
@@ -91,4 +90,81 @@ function updateKeys() {
 */
 function css( element, property ) {
     return window.getComputedStyle( element, null ).getPropertyValue( property );
+}
+
+
+/*
+* Preload function
+*/
+function preload(images) {
+    for (var i = images.length - 1; i >= 0; i--) {
+        var img = new Image();
+        img.src = 'img/'+images[i];
+    };
+}
+
+
+/*
+* Creates a new obstacle
+*/
+function createObstacle() {
+    var element = document.createElement('img');
+    element.src = 'img/' + obstacleImg[random(0, obstacleImg.length-1)];
+    var imgStyle = element.style;
+    imgStyle.position = 'absolute';
+    imgStyle.bottom = '100%';
+    imgStyle.width = '90px';
+    imgStyle.left = random(0, parseInt(css(window.gameWindow, 'width'), 10) - parseInt(imgStyle.width, 10) ) + 'px';
+    return element;
+}
+
+
+/*
+* Creates a new obstacle
+* Adds it to the game window
+* Starts moving it
+*/
+function addObstacle() {
+    var ob = createObstacle();
+    window.gameWindow.appendChild(ob);
+    moveObstacle(ob, travelSpeed, travelInterval);
+}
+
+
+/*
+*
+*/
+function addRandomObstacles() {
+    setTimeout(function() {
+        addRandomObstacles();
+    }, random(500, 1000));
+    addObstacle();
+    // addObstacle();
+}
+
+
+/*
+* Move the obstacle untill it can't be seen anymore
+* and then removes it
+*/
+function moveObstacle(ob, travelSpeed, interval) {
+    var oldBottom = parseInt(css(ob, 'bottom'), 10);
+    var height = parseInt(css(ob, 'height'), 10);
+    if (oldBottom <= -height) {
+        window.gameWindow.removeChild(ob);
+        return;
+    }
+    setTimeout(function() {
+        moveObstacle(ob, travelSpeed, interval);
+    }, interval);
+    var newBottom = oldBottom - travelSpeed;
+    ob.style.bottom = newBottom + 'px';
+}
+
+
+/*
+* Return a random number between min and max
+*/
+function random(min, max) {
+    return Math.floor((Math.random() * max) + min); 
 }
